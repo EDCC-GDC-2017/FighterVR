53293ed275d99b5de433b3099d76ee7855caec5d
JustinWeq <zoomdude111@hotmail.com>
2017-04-18 14:52:08 -0700

    Initial commit

---
+++ Source/FighterVR/FighterVRGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FighterVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTERVR_API AFighterVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
